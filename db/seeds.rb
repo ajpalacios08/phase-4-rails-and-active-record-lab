80f7cc703f91d2500748024498ee293553021424
Allen <apalacios1994@gmail.com>
2021-07-19 14:57:24 -0500

    commit tree 788aa687676d712bf0418ac8aab666860926c8bc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!([{ first_name: 'Chad', last_name: 'Little', grade: 6}])
+Student.create!([{ first_name: 'Brenda', last_name: 'Escobar', grade: 9}])
+Student.create!([{ first_name: 'Nancy', last_name: 'Lopez', grade: 11}])
+Student.create!([{ first_name: 'Brayan', last_name: 'Ramoz', grade: 2}])
+Student.create!([{ first_name: 'Johan', last_name: 'Perez', grade: 8}])
